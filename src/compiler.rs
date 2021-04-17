933a56621e61489a76b1747cf765950e49f32bbe
Wafelack <wafelack@protonmail.com>
2021-04-17 10:48:17 +0200

    license: Added copyright notice.

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,3 +1,22 @@
+/*
+ *  GVLC is the Gentle Vim Lisp Compiler.
+ *  Copyright (C) 2021  Wafelack
+ * 
+ *  This file is part of GVLC.
+ *
+ *  GVLC is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  GVLC is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GVLC.  If not, see <https://www.gnu.org/licenses/>.
+ */
 use crate::{parser::{ExprT, Expr}, VLispResult};
 
 mod map;
